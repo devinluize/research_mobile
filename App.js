1fbf94e7b4926c88050974916a13da5e514ddbd5
devinluize <71080055+devinluize@users.noreply.github.com>
2023-06-10 22:21:46 +0700

    add api jsx and forum jsx

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import RadioButton from "./RadioButton";
+import { login } from "./ForumApi";
 export default function App() {
+  const data = [
+    { value: "Apple" },
+    { value: "Samsung" },
+    { value: "Blackberry" },
+  ];
+  const Login = async () => {
+    const email = "elys@gmail.com";
+    const password = "elys123";
+    const response = login({ email, password });
+    console.log(response);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text style={styles.paragraph}>Choose your favorite company: </Text>
+      <RadioButton data={data} /> */}
+      <Button title="login" onPress={() => Login()}>
+        tes
+      </Button>
     </View>
   );
 }
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
